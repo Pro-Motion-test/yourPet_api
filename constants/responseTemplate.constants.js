49d5089da647335d75c7b7910d895d8365c1d16f
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-05-20 23:53:00 +0300

    fix

---
+++ constants/responseTemplate.constants.js
@@ -0,0 +1,29 @@
+const SUCCESS_GET_RESPONSE = {
+  status: 'Success',
+  code: 200,
+  message: 'Successfully processed!',
+};
+
+const SUCCESS_POST_RESPONSE = {
+  status: 'Success',
+  code: 201,
+  message: 'Successfully created!',
+};
+const SUCCESS_DELETE_RESPONSE = {
+  status: 'Success',
+  code: 204,
+  message: 'Successfully deleted!',
+};
+
+const SUCCESS_PUT_RESPONSE = {
+  status: 'Success',
+  code: 200,
+  message: 'Successfully updated!',
+};
+
+module.exports = {
+  SUCCESS_DELETE_RESPONSE,
+  SUCCESS_POST_RESPONSE,
+  SUCCESS_GET_RESPONSE,
+  SUCCESS_PUT_RESPONSE,
+};
