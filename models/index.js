b2a995293c5a93b96b3461cc386b8b2571ac217d
Yuliia Skobel <106476498+Skobel58912171@users.noreply.github.com>
2023-05-23 14:26:05 +0300

    fix

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const { User, schemas: userSchemas } = require('./user.model');
 const { Notice, schemas: noticeSchemas } = require('./notice.model');
 const { Pet, schemas: petSchemas } = require('./pet.model');
 
-const { News } = require('./news.model');
+const {News} = require('./news.model');
 
 const models = { User, Notice, Pet, News };
 const schemas = { userSchemas, noticeSchemas, petSchemas };
